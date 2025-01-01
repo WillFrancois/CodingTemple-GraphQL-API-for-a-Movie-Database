9900a234ebb54a82e594e0159f4ac323f356b591
WillFrancois <williamjcfrancois@gmail.com>
2024-12-31 20:17:16 -0500

    Added README and missing mutation functions

---
+++ README.md
@@ -0,0 +1,35 @@
+# GraphQL API For A Movie Database
+
+This project allows users to manipulate a MySQL database using GraphiQL to perform queries and mutations directly from their browser to their database.
+
+## Database Set-up
+
+To set up the database for this project to work, create a MySQL database on port 3306 with the password "CodingTemple" for the root user. After this, create a database named "movie_db".
+
+## Python Environment Set-up
+
+To create the Python environment needed for this code, run the command "python -m venv (your venv folder name here)" and use the virtual environment's pip to install the dependencies from the requirements.txt file.
+
+## Running the Code
+
+To run the code properly after installing the dependencies, use the flask binary available in your virtual environment's bin folder to deploy the server.
+
+## Accessing the GraphiQL Dashboard
+
+The GraphiQL dashboard is available from your web browser. Access the port your flask server is running on and add '/graphql' to start running commands.
+
+### Queries:
+
+movies: Gives all movies available in the database.
+genres: Gives all genres available in the database.
+getMoviesByGenre: A function that takes a genreId as input and produces all movies with the same genre.
+getGenreByMovie: A function that takes a movieId as input and produces the related genre.
+
+### Mutations:
+
+createMovies: Creates a movie and inserts it into the database.
+createGenre: Creates a genre and inserts it into the database.
+updateMovies: Updates a movie in the database from the given information.
+updateGenre: Updates a genre in the database from the given information.
+deleteMovies: Deletes a movie in the database from a given id number.
+deleteGenre: Deletes a genre in the database from a given id number.
--- schema.py
+++ schema.py
@@ -14,6 +14,24 @@ class Genre(SQLAlchemyObjectType):
     class Meta:
         model = GenreModel
 
+class CreateMovies(graphene.Mutation):
+    class Arguments:
+        title = graphene.String(required=True)
+        description = graphene.String(required=True)
+        release_year = graphene.Date(required=True)
+        genre_id = graphene.Int(required=True)
+
+    movie = graphene.Field(Movie)
+
+    def mutate(self, info, title, description, release_year, genre_id):
+        with Session(db.engine) as session:
+            with session.begin():
+                movie = MovieModel(title=title, description=description, release_year=release_year, genre_id=genre_id)
+                session.add(movie)
+
+            session.refresh(movie)
+            return CreateMovies(movie=movie)
+
 class CreateGenre(graphene.Mutation):
     class Arguments:
         name = graphene.String(required=True)
@@ -32,6 +50,31 @@ class CreateGenre(graphene.Mutation):
 
             session.refresh(genre)
             return CreateGenre(genre=genre)
+
+class UpdateMovies(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+        title = graphene.String(required=True)
+        description = graphene.String(required=True)
+        release_year = graphene.Date(required=True)
+        genre_id = graphene.Int(required=True)
+
+    movie = graphene.Field(Movie)
+
+    def mutate(self, info, id, title, description, release_year, genre_id):
+        with Session(db.engine) as session:
+            with session.begin():
+                exists = session.execute(select(MovieModel).where(MovieModel.id==id)).first()
+                if exists:
+                    movie = MovieModel(id=id, title=title, description=description, release_year=release_year, genre_id=genre_id)
+                else:
+                    movie = MovieModel(id=0, title="An error has occured", description="An error has occured", release_year="1970-01-01", genre_id=0)
+                    return UpdateMovies(movie=movie)
+
+            session.merge(movie)
+            session.commit()
+            return UpdateMovies(movie=movie)
+
 
 class UpdateGenre(graphene.Mutation):
     class Arguments:
@@ -53,6 +96,25 @@ class UpdateGenre(graphene.Mutation):
             session.merge(genre)
             session.commit()
             return UpdateGenre(genre=genre)
+
+class DeleteMovies(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required = True)
+
+    ok = graphene.Boolean()
+
+    def mutate(self, info, id):
+        with Session(db.engine) as session:
+            with session.begin():
+                exists = session.execute(select(MovieModel).where(MovieModel.id==id)).first()
+                if exists:
+                    session.execute(delete(MovieModel).where(MovieModel.id==id))
+                    ok = True
+                else:
+                    ok = False
+
+            session.commit()
+            return DeleteMovies(ok)
 
 class DeleteGenre(graphene.Mutation):
     class Arguments:
@@ -94,6 +156,9 @@ class Query(graphene.ObjectType):
         return db.session.execute(db.select(GenreModel).where(GenreModel.id == gen_id)).first()[0]
 
 class Mutation(graphene.ObjectType):
+    create_movies = CreateMovies.Field()
     create_genre = CreateGenre.Field()
+    update_movies = UpdateMovies.Field()
     update_genre = UpdateGenre.Field()
+    delete_movies = DeleteMovies.Field()
     delete_genre = DeleteGenre.Field()
